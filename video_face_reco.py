8b55416b914933d64d7d761753e993b68e9523a3
Gara64 <paul@cozycloud.cc>
2018-02-05 01:42:34 +0100

    Add video face reco

---
+++ video_face_reco.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import face_recognition
+import cv2
+import click
+import pickle
+
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+
+MODEL_FILE = "model.pkl"
+
+@click.command()
+@click.option('--model', default=MODEL_FILE, help='Face model to use')
+def main(model):
+    # Extract known faces from model
+    model_encoded_images = []
+    model_face_names = []
+    print("open model %s" % model   )
+    with open(model, 'rb') as input:
+        encodedFaces = pickle.load(input)
+        print("faces found in model : %s " % encodedFaces.keys())
+
+        for k, v in encodedFaces.iteritems():
+            model_face_names.append(k)
+            model_encoded_images.append(v)
+
+
+    process_this_frame = True
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(small_frame)
+            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(model_encoded_images, face_encoding, 0.55)
+                faceFound = False
+                for i, res in enumerate(matches):
+                    if res:
+                        print("%s match!" % model_face_names[i])
+                        face_names.append(model_face_names[i])
+                        faceFound = True
+                        break
+                if not faceFound:
+                    face_names.append("Unknown")
+
+        process_this_frame = not process_this_frame
+
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.cv.CV_FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()

0d543f7b241c7d10949f5824e52e1125c00cfde7
Gara64 <paul@cozycloud.cc>
2018-03-28 13:52:38 +0200

    Run reco when addind new face

--- scripts/auto_add_face.py
+++ scripts/auto_add_face.py
@@ -3,18 +3,57 @@ import sys
 import os
 import click
 import time
+import json
+from stat import S_ISREG, ST_CTIME, ST_MODE
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), "../"))
 from model import add_face
+from photo_face_reco import recognize
+from photo_face_reco import image_files_in_folder
+from share import upload_and_share
+from share import remote_face_copy
 
 # This script listens to new faces to add in the model
 
 
-class Model(object):
-    def __init__(self, model):
-        Model.model = model
+def list_dir_by_date(dir_path):
+    dirs = (os.path.join(dir_path, fn) for fn in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, fn)))
+    dirs = ((os.stat(path), path) for path in dirs)
+    dirs = ((stat[ST_CTIME], path) for stat, path in dirs)
+
+    sorted_dirs = [path for time, path in sorted(dirs, reverse=True)]
+    return sorted_dirs
+
+def count_unknown(dir_path):
+    cpt = 0
+    for fn in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, fn)
+        ext = os.path.splitext(file_path)[1]
+        if ext == ".json":
+            with open(file_path) as f:
+                data = json.load(f)
+                subjects = data['subjects']
+                for s in subjects:
+                    if s == "unknown":
+                        cpt = cpt + 1
+    return cpt
+
+
+def get_photo_path_in_dir(dir_path):
+    imgs = image_files_in_folder(dir_path)
+    for img in imgs:
+        if "_reco" not in img:
+            return img
+
+
+class Params(object):
+    def __init__(self, model, reco_path, instance, remote_server):
+        Params.model = model
+        Params.reco_path = reco_path
+        Params.instance = instance
+        Params.remote_server = remote_server
 
 class Handler(PatternMatchingEventHandler):
     patterns=["*.jpg"]
@@ -33,15 +72,36 @@ class Handler(PatternMatchingEventHandler):
             path/to/observed/file
         """
 
-        add_face(event.src_path, Model.model)
+        add_face(event.src_path, Params.model)
+        print("src path %s" % event.src_path)
+        remote_face_copy(event.src_path, Params.instance, Params.remote_server )
+
+        # Let's check photos with the updated model
+        if Params.reco_path != "":
+            dirs = list_dir_by_date(Params.reco_path)
+            photo_face_basename = os.path.basename(event.src_path)
+
+            for d in dirs:
+                print("check dir %s with this new face" % d)
+                n_unknown = count_unknown(d)
+                if n_unknown > 0:
+                    photo_path = get_photo_path_in_dir(d)
+                    photo_name = os.path.splitext(os.path.basename(photo_path))[0]
+
+                    recognize(photo_path, Params.model, 0.53, Params.reco_path, True)
+                    print('photo name : %s' % photo_name)
+                    upload_and_share(photo_name, d, Params.instance, Params.remote_server)
 
 
 @click.command()
 @click.argument('faces_path')
 @click.argument('model')
-def main(faces_path, model):
+@click.option('--reco_path', default='', help='Path of the photo directory on which running the recognition. No path means no reco.')
+@click.option('--instance', default='cozy1.local:8080', help='Cozy instance')
+@click.option('--remote_server', default='', help='Remote server <user>@<server_host>')
+def main(faces_path, model, reco_path, instance, remote_server):
 
-    Model(model)
+    Params(model, reco_path, instance, remote_server)
     observer = Observer()
     observer.schedule(Handler(), path=faces_path)
     observer.start()
